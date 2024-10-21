891d76bdfad53d867146e2434b0eac6dd7e70888
shubham <shubham.sharma23105@gmail.com>
2024-10-21 19:26:43 +0530

    commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// ------------ Navbar ------------
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
