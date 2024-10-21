28555437be17aea41880940a6257cd96ecf3af61
shubham <shubham.sharma23105@gmail.com>
2024-10-21 21:39:26 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ const navLinks = document.getElementById('navLinks');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+var sidebar = document.querySelector('#sidebar')
+var buyBtn = document.querySelector('#buy-btn');
+
+buyBtn.addEventListener('click',()=>{
+    sidebar.style.display='block';
+})
+
